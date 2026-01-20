d3aba63af06f1a95edae80dce4fd7fd8b4faf57c
sreya havappanoor <havappanoorsreya@gmail.com>
2026-01-20 18:45:19 +0530

    Create IfElse.java

---
+++ basics/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n>0) {
+      System.out.prinlnt("Positive Number");
+    } else {
+      System.out.println("Negative Number");
+    }
+  }
+}
