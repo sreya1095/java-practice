3c50ca54341a50695a0a7a0c23dec5ae8bfe0394
sreya1095 <havappanoorsreya@gmail.com>
2026-02-04 21:28:20 +0530

    Added method overloading example

---
+++ oops/MethodOverloading.java
@@ -0,0 +1,22 @@
+class Calculator {
+
+    // Method with 2 parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with 3 parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of 2 numbers: " + calc.add(10, 20));
+        System.out.println("Sum of 3 numbers: " + calc.add(5, 10, 15));
+    }
+}
