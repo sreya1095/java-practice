4a6e05a4bcc31a35cd2f0de43a0543e91e02b25b
sreya1095 <havappanoorsreya@gmail.com>
2026-02-06 20:18:01 +0530

    Added abstraction example using abstract class

---
+++ oops/AbstractionDemo.java
@@ -0,0 +1,21 @@
+abstract class Shape {
+    abstract void draw();
+
+    void display() {
+        System.out.println("This is a shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+public class AbstractionDemo {
+    public static void main(String[] args) {
+        Shape s = new Circle();
+        s.display();
+        s.draw();
+    }
+}
