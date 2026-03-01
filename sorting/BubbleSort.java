b580227d462950100989408a01df19ee41d66bf9
sreya1095 <havappanoorsreya@gmail.com>
2026-03-01 23:06:13 +0530

    Added BubbleSort initial implementation

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,11 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 9, 1};
+
+        // TODO: Implement bubble sort logic
+
+        System.out.println("Sorting in progress...");
+    }
+}
