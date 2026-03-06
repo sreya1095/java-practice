a49498547cacfdba9b953a70e48dec3198c67a7d
sreya1095 <havappanoorsreya@gmail.com>
2026-03-06 18:43:45 +0530

    Added ArmstrongNumber in basics dir

---
+++ basics/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+package basics;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while(num > 0) {
+
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if(sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not an Armstrong Number");
+    }
+}
